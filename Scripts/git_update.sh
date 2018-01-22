4f9483ffe6f57993f429a60398ae1ac874c1fbab
krzysztof.siejkowski <krzysztof.siejkowski@polidea.com>
2018-01-22 10:15:17 +0100

    Updated git update script to prevent build artifacts invalidation

--- Scripts/git_update.sh
+++ Scripts/git_update.sh
@@ -7,17 +7,19 @@ projects=(
   "VerificationSuite"
 )
 
-cp -r -p SymbolExtractorAndRenamer/build ./tmp_build
-rm -r SymbolExtractorAndRenamer/build
-
 for i in ${!projects[@]}; do
-  git rm -r ${projects[$i]} --quiet
+  echo "Fetch origin ${projects[$i]}"
   git fetch ${projects[$i]}
-  git read-tree --prefix=${projects[$i]} -u ${projects[$i]}/master
-done
 
-cp -r -p tmp_build SymbolExtractorAndRenamer/build 
-rm -r tmp_build
+  echo "Clone code from ${projects[$i]} to directory ${projects[$i]}_tmp"
+  git read-tree --prefix=${projects[$i]}_tmp -u ${projects[$i]}/master
+
+  echo "Copy cloned code from ${projects[$i]}_tmp to ${projects[$i]}"
+  rsync --progress --checksum --ignore-times -r ${projects[$i]}_tmp/* ${projects[$i]}
+
+  echo "Delete directory ${projects[$i]}_tmp"
+  rm -r "${projects[$i]}_tmp"
+done
 
 git add . -A
 git commit -m "Updated dependencies"
